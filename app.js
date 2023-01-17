38a26cd7a911d6ac8521b57a12e8c40f7b8a639a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 21:45:05 +0300

    Used proper hexadecimal notation for the material

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const scene = new THREE.Scene()
 
 // Red cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
