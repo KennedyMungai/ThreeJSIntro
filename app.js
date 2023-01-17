9597945a10bc62be34002f741ce4a275fbf2becb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-17 21:46:10 +0300

    Defined the mesh

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ const scene = new THREE.Scene()
 // Red cube
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
