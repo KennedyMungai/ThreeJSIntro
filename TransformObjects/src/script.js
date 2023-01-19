fb6416647f4110ba4533d9c11bf6661cb30e1fc9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-19 09:57:18 +0300

    Changed the position and orientation of the camera

--- TransformObjects/src/script.js
+++ TransformObjects/src/script.js
@@ -33,7 +33,7 @@ const sizes = {
  * Camera
 */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.set(1, 1, 3)
 scene.add(camera)
 
 /**
