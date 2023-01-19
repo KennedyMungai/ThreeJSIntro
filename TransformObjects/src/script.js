71ea2993c211ac3dde014f17d52ea4b0883c4da8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-19 09:35:25 +0300

    Moved the cube 1 unit in the Y axis

--- TransformObjects/src/script.js
+++ TransformObjects/src/script.js
@@ -10,6 +10,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.position.y = 1
 scene.add(mesh)
 
 /**
