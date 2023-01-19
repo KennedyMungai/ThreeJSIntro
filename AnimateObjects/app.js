1950636843d05b31b609f94737c23a876f8bbbe1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-19 11:27:09 +0300

    Created a new Three js camera and added it to the scene

--- AnimateObjects/app.js
+++ AnimateObjects/app.js
@@ -5,3 +5,11 @@ const group = new THREE.Group()
 
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
+
+const size = {
+    x: 800,
+    y: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, size.y / size.x)
+scene.add(camera)
