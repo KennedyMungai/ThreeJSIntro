35daf03822b0f6f5e9b61d374f0dd07857bd13ce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-19 11:25:17 +0300

    Created and added an axesHelper object to the scene

--- AnimateObjects/app.js
+++ AnimateObjects/app.js
@@ -2,3 +2,6 @@ const canvas = document.getElementById('webgl')
 
 const scene = new THREE.Scene()
 const group = new THREE.Group()
+
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
