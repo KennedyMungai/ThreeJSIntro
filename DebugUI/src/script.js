2076c7139b3566c40ab1419938a96a22dd5ce312
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-20 11:45:19 +0300

    Used different syntax to define the range

--- DebugUI/src/script.js
+++ DebugUI/src/script.js
@@ -25,9 +25,9 @@ const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // Debug
-gui.add(mesh.position, 'x', -3, 3, 0.01)
-gui.add(mesh.position, 'y', -3, 3, 0.01)
-gui.add(mesh.position, 'z', -3, 3, 0.01)
+// gui.add(mesh.position, 'x', -3, 3, 0.01)
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01)
+// gui.add(mesh.position, 'z', -3, 3, 0.01)
 
 /**
  * Sizes
