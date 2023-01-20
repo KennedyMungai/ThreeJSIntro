378720c6985511b7367218e2ba874eb6b76fc5fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-20 09:49:29 +0300

    Changed the far clip value to 1000

--- FullscreenCanvas/src/script.js
+++ FullscreenCanvas/src/script.js
@@ -55,7 +55,7 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 camera.position.z = 3
 scene.add(camera)
 
