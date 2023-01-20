22e9557c3da7abc5f3c726c8029f6a99da993f25
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-20 11:16:47 +0300

    Used the individual values to fill  in the positionsArray

--- Geometries/src/script.js
+++ Geometries/src/script.js
@@ -22,19 +22,9 @@ const gridHelper = new THREE.GridHelper(50, 50, 'red', 'gray')
 // Object
 // const geometry = new THREE.BoxGeometry(1, 1, 1)
 
-const positionsArray = new Float32Array(9)
-
-positionsArray[0] = 0
-positionsArray[1] = 0
-positionsArray[2] = 0
-
-positionsArray[3] = 0
-positionsArray[4] = 1
-positionsArray[5] = 0
-
-positionsArray[6] = 1
-positionsArray[7] = 0
-positionsArray[8] = 0
+const positionsArray = new Float32Array([
+    0, 0, 0, 0, 1, 0, 1, 0, 0
+])
 
 const material = new THREE.MeshBasicMaterial(
     {
