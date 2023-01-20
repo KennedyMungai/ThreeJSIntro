55d6ab7618c951a39af7a87a540945ba1a5cb4a1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-20 12:41:58 +0300

    Switched from using color to using map

--- Textures/src/script.js
+++ Textures/src/script.js
@@ -27,7 +27,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial(
     {
-        color: 0xff0000
+        map: true
     }
 )
 const mesh = new THREE.Mesh(geometry, material)
