2e4821e98ba202048de5d00a0aecff78605708fe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-20 10:29:23 +0300

    Added side segments for the box shape

--- Geometries/src/script.js
+++ Geometries/src/script.js
@@ -20,7 +20,7 @@ const gridHelper = new THREE.GridHelper(50, 50, 'red', 'gray')
 scene.add(gridHelper)
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = new THREE.BoxGeometry(1, 2, 3, 2, 2, 2)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
