49347c55ea5694326e584c7e7d96b6601a42e135
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-20 12:45:24 +0300

    Created a new instance of a texture loader

--- Textures/src/script.js
+++ Textures/src/script.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Textures
-
+const textureLoader = new THREE.TextureLoader()
 
 /**
  * Base
