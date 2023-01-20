7aba2d761d4f7a520c5e6f6be47b02cb2e4341fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-20 15:07:30 +0300

    Commented out some texture manipulation code

--- Textures/src/script.js
+++ Textures/src/script.js
@@ -35,11 +35,18 @@ const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclus
 const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
-colorTexture.repeat.x = 2
-colorTexture.repeat.y = 3
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// 
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+// 
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
 
-colorTexture.wrapS = THREE.RepeatWrapping
-colorTexture.wrapT = THREE.RepeatWrapping
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
 
 /**
  * Base
