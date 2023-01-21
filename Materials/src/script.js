4d7f4458d2ed86a44b1fb965e9d422cbd782f1ed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-21 12:12:25 +0300

    Commented out the metalness and roughness GUI properties

--- Materials/src/script.js
+++ Materials/src/script.js
@@ -78,17 +78,17 @@ material.alphaMap = doorAlphaTexture
 material.metalnessMap = doorMetalnessTexture
 material.roughnessMap = doorRoughnessTexture
 
-gui
-    .add(material, 'metalness')
-    .min(0)
-    .max(1)
-    .step(0.01)
+// gui
+//     .add(material, 'metalness')
+//     .min(0)
+//     .max(1)
+//     .step(0.01)
 
-gui
-    .add(material, 'roughness')
-    .min(0)
-    .max(1)
-    .step(0.01)
+// gui
+//     .add(material, 'roughness')
+//     .min(0)
+//     .max(1)
+//     .step(0.01)
 
 gui
     .add(material, "aoMapIntensity")
