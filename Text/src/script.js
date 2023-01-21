41e00ba127b0109ce87305e4562652d79a1b5007
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-21 19:55:54 +0300

    Added a grid help[er to the scene

--- Text/src/script.js
+++ Text/src/script.js
@@ -19,6 +19,10 @@ const scene = new THREE.Scene()
 const axesaHelper = new THREE.AxesHelper(50)
 scene.add(axesaHelper)
 
+// GridHelper
+const gridHelper = new THREE.GridHelper(50, 50, 'red', 'gray')
+scene.add(gridHelper)
+
 /**
  * Textures
  */
