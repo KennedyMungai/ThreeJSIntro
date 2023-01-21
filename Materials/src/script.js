44d0a07f6ed931fb48513a77d83331a2fbc716f7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-21 10:50:34 +0300

    Applied the normal basic material to teh items

--- Materials/src/script.js
+++ Materials/src/script.js
@@ -34,9 +34,9 @@ scene.add(axesHelper)
 const gridHelper = new THREE.GridHelper(50, 50, 'red', 'gray')
 scene.add(gridHelper)
 
-// const material = new THREE.MeshBasicMaterial()
+const material = new THREE.MeshBasicMaterial()
 // material.map = doorColorTexture
-// material.color = new THREE.Color('magenta')
+material.color = new THREE.Color('magenta')
 // material.color.set('magenta')
 // material.wireframe = true
 // material.transparent = true
