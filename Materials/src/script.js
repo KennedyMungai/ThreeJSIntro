76311ef2efb2b5e49254a363bdd99d3bb7cd5f9f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-21 10:55:36 +0300

    Assigned a new position for the point light

--- Materials/src/script.js
+++ Materials/src/script.js
@@ -82,6 +82,7 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
 const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.set(2, 3, 4)
 scene.add(pointLight)
 
 /**
