c9855fb72bc99179d70b227dbeba2aae9208694d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-21 20:05:05 +0300

    Implemented the code for 3D text

--- Text/src/script.js
+++ Text/src/script.js
@@ -33,9 +33,26 @@ const fontloader = new THREE.FontLoader()
 
 fontloader.load(
     '/fonts/helvetiker_regular.typeface.json',
-    () => 
+    (font) => 
     {
-        console.log("Font loaded")
+        const textGeometry = new THREE.TextBufferGeometry(
+            'Sent from heaven',
+            {
+                font,
+                size: 0.5,
+                height: 0.5,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0xfff000 })
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text)
     }
 )
 
