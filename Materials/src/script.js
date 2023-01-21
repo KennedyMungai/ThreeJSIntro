b23216bbc1648435cbee7be7cb0e9fba2db233ab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-21 12:09:48 +0300

    Added a gui control for ao map intensity

--- Materials/src/script.js
+++ Materials/src/script.js
@@ -90,6 +90,12 @@ gui
     .max(1)
     .step(0.01)
 
+gui
+    .add(material, "aoMapIntensity")
+    .min(1)
+    .max(5)
+    .step(0.1)
+
 // Added a sphere
 const sphere = new THREE.Mesh(
     new THREE.SphereBufferGeometry(0.5, 16, 16),
