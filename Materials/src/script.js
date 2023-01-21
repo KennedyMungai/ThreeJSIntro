692e46ba1e7d1015375cf61710f19b70392b7866
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-21 11:39:57 +0300

    Instantiated dat.GUI

--- Materials/src/script.js
+++ Materials/src/script.js
@@ -3,6 +3,9 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// DEBUG
+const gui = new dat.GUI()
+
 /**
  * Base
  */
