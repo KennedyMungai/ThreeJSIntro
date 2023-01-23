cefa828bd049f1de72577d0e32760d513a7b211d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-23 12:08:08 +0300

    Disabled the ambient light and the point light

--- Lights/src/script.js
+++ Lights/src/script.js
@@ -19,13 +19,13 @@ const scene = new THREE.Scene()
  * Lights
  */
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
+// scene.add(ambientLight)
 
 const pointLight = new THREE.PointLight(0xffffff, 0.5)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
-scene.add(pointLight)
+// scene.add(pointLight)
 
 /**
  * Objects
