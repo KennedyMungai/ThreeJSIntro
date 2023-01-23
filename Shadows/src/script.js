064b457ceff41ca7bd2259ddd4e4d14efdca9dcc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-23 15:38:13 +0300

    Did a proper shadow deactivation

--- Shadows/src/script.js
+++ Shadows/src/script.js
@@ -155,8 +155,8 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// renderer.shadowMap.enabled = true
-// renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.shadowMap.enabled = false
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 /**
  * Animate
