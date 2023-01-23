a41fa7c44c4b11f3cca80d06c56b2578c0de10ea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-23 12:08:40 +0300

    Removed the lights much more thoroughly

--- Lights/src/script.js
+++ Lights/src/script.js
@@ -18,13 +18,13 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 // scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+// const pointLight = new THREE.PointLight(0xffffff, 0.5)
+// pointLight.position.x = 2
+// pointLight.position.y = 3
+// pointLight.position.z = 4
 // scene.add(pointLight)
 
 /**
