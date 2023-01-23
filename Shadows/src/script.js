716ec3ccb76a065a75fb6b89f8519d09db17a0af
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-23 14:43:14 +0300

    Halved the value of the far parameter

--- Shadows/src/script.js
+++ Shadows/src/script.js
@@ -36,8 +36,12 @@ directionalLight.castShadow = true
 
 directionalLight.shadow.mapSize.width = 1024
 directionalLight.shadow.mapSize.height = 1024
+// directionalLight.shadow.camera.top = 2
+// directionalLight.shadow.camera.right = 2
+// directionalLight.shadow.camera.bottom = 2
+// directionalLight.shadow.camera.left = 2
+directionalLight.shadow.camera.far = 5
 directionalLight.shadow.camera.near = 1
-directionalLight.shadow.camera.far = 10
 
 const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
 scene.add(directionalLightCameraHelper)
