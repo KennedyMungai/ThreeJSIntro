82e55732a9eece2740cc0dfeb73ee5b098d427f0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-24 10:46:40 +0300

    Changed the radius' magnitude

--- HauntedHouse/src/script.js
+++ HauntedHouse/src/script.js
@@ -180,7 +180,7 @@ const graveMaterial = new THREE.MeshStandardMaterial({ color: 0xb2b6b1 })
 for (let i = 0; i < 50; i++)
 {
     const angle = Math.random() * (Math.PI * 2)
-    const radius = 3 + Math.random() * 6
+    const radius = 7.5 + Math.random() * 6
     const x = Math.sin(angle) * radius
     const z = Math.cos(angle) * radius
 
