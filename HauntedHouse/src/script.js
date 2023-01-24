1d62bf24c21b37e04ba34ea95b974192cc4b3fba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-24 12:11:51 +0300

    Gave the second ghost a wider berth when it comes to mevement

--- HauntedHouse/src/script.js
+++ HauntedHouse/src/script.js
@@ -341,8 +341,8 @@ const tick = () =>
     ghost1.position.y = Math.sin(elapsedTime * 2) * 2
 
     const ghost2Angle = -elapsedTime * 0.32
-    ghost2.position.x = Math.cos(ghost1Angle) * 6
-    ghost2.position.z = Math.sin(ghost1Angle) * 6
+    ghost2.position.x = Math.cos(ghost1Angle) * 7.5
+    ghost2.position.z = Math.sin(ghost1Angle) * 7.5
     ghost2.position.y = Math.sin(elapsedTime * 2) * 2
 
     // Update controls
