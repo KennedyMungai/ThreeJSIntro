8d463599fc682ec7b0241a3d3076ce4167e05162
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-25 19:57:07 +0300

    Applied a local force to the object

--- Physics/src/script.js
+++ Physics/src/script.js
@@ -64,6 +64,8 @@ const sphereBody = new CANNON.Body({
     shape: sphereShape,
 })
 
+sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0))
+
 world.addBody(sphereBody)
 
 // Floor
