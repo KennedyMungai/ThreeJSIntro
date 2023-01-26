cce349dc90fffb6933c2eaf53a8cfcae81c7c433
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-26 09:09:12 +0300

    Used sweep and prune as the broadface method

--- Physics/src/script.js
+++ Physics/src/script.js
@@ -68,6 +68,7 @@ const environmentMapTexture = cubeTextureLoader.load([
  */
 // World
 const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
 world.gravity.set(0, -9.82, 0)
 
 // Materials
