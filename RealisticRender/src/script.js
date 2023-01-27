8918b1edaf5e6a454ba8393c0c36ec55cb2d0f65
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 09:08:31 +0300

    Loaded the cubemap

--- RealisticRender/src/script.js
+++ RealisticRender/src/script.js
@@ -23,6 +23,18 @@ const gltfLoader = new GLTFLoader()
 const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 /**
+ * Environment Textures
+ */
+const environmentMap = cubeTextureLoader.load(
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg',
+)
+
+/**
  * Models
  */
 gltfLoader.load(
