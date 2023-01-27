da7f67fb0fdad9db9ebbd04fa3a82ea0043fc136
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 15:21:45 +0300

    Combined two variables into one

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -1,9 +1,8 @@
 uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
 
 attribute vec3 position;
 
 void main() {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
