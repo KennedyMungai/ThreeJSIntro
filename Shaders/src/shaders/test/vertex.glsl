b796829082cc50b39c205fb6ddedbe2374883fb3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 15:56:19 +0300

    Deleted the commented out code

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -10,7 +10,6 @@ varying float vRandom;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
     modelPosition.z += aRandom * 0.1;
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
