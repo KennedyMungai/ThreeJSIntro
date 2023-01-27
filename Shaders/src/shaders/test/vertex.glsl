be3147bccf32eb08b39558f59fdcd230a54b49ae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 15:31:08 +0300

    Changed the multiplier back to 10

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -7,7 +7,7 @@ attribute vec3 position;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.z += sin(modelPosition.x * 20.0) * 0.1;
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
