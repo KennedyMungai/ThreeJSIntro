3a78ac1bc8283019763e225794707e01634c1013
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 15:29:53 +0300

    Made the place look wavy

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -7,6 +7,8 @@ attribute vec3 position;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
