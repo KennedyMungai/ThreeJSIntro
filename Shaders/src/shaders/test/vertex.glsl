60c07de08b7c417afa9cd657994a35b0a9a6fd6c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 15:27:44 +0300

    Removed the transformation line of code in the shader file

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -7,8 +7,6 @@ attribute vec3 position;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.y += 1.0;
-
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
