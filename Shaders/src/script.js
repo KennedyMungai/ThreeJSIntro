e7199bd4dd9331cc2e182efb15a319d5871cb21a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 14:49:09 +0300

    Assigned the shader files to the shader variables

--- Shaders/src/script.js
+++ Shaders/src/script.js
@@ -30,8 +30,8 @@ const geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32)
 
 // Material
 const material = new THREE.RawShaderMaterial({
-    vertexShader: ``,
-    fragmentShader: ``
+    vertexShader: testVertexShader,
+    fragmentShader: testFragmentShader
 })
 
 // Mesh
