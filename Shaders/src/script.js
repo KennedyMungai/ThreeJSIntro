482f2db7121449a2685cea521c1b3437fa529144
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 14:40:47 +0300

    Added imports fro both the vertex and fragment shaders

--- Shaders/src/script.js
+++ Shaders/src/script.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import testVertexShader from './shaders/test/vertex.glsl'
+import testFragmentShader from './shaders/test/fragment.glsl'
 
 /**
  * Base
