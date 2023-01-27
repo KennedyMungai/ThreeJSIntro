9110a8c13cffbac10ae1e59083c80278393ae279
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-27 15:44:21 +0300

    Commented out the model z sine wave attribute

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -8,7 +8,7 @@ attribute float aRandom;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
