325edaffb59c22bb38ba57782047c12dd4690e2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 13:37:07 +0300

    Implemented the banded revresed on th ey-axis

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -5,7 +5,10 @@ void main() {
     // float strength = vUv.x;
 
     // Gray scale banded pattern on the y axis
-    float strength = vUv.y;
+    // float strength = vUv.y;
+
+    // Gray scale banded pattern on the y axis inverted
+    float strength = 1.0 - vUv.y;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
