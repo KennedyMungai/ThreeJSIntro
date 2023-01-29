1205aeeeb2d8d803231ae27ca1992dfb7405da1b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 13:34:59 +0300

    Some simple refactoring

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -2,6 +2,9 @@ varying vec2 vUv;
 
 void main() {
     // Gray scale banded pattern
+    // float strength = vUv.x;
+
+    // Gray scale banded pattern on the y axis
     float strength = vUv.y;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
