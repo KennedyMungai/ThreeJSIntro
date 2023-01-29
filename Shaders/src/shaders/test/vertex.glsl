5b16ccef2e08aea0a7c69fc9486815f9ec322745
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 11:06:59 +0300

    Added another defoirmation yo change the z based on the y

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -11,7 +11,8 @@ attribute vec3 position;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.z += sin(modelPosition.x * uFrequency) * 0.1;
+    modelPosition.z += sin(modelPosition.x * uFrequency.x) * 0.1;
+    modelPosition.z += sin(modelPosition.y * uFrequency.x) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
