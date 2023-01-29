390cd3286ebcef96580f7bfa2e85f9ba9d633003
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 18:01:49 +0300

    Experimented with colored Perlin noise

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -221,7 +221,13 @@ void main() {
     // float strength = sin(cnoise(vUv * 10.0) * 20.0);
 
     // Another weird looking perlin noise pattern but sharper
-    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+    // float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
 
-    gl_FragColor = vec4(vec3(strength), 1.0);
+    // The pattern above only coloured
+    // float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+    float strength = cnoise(vUv * 10.0);
+    vec3 blackColor = vec3(0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    vec3 mixedColor = mix(blackColor, uvColor, strength);
+    gl_FragColor = vec4(mixedColor, 1.0);
 }
