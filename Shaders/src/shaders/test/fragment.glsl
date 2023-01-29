254b7a59920d4c3ee67256207b69e209e5eaf918
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 11:43:46 +0300

    Added the texture coord information

--- Shaders/src/shaders/test/fragment.glsl
+++ Shaders/src/shaders/test/fragment.glsl
@@ -3,9 +3,11 @@ precision mediump float;
 uniform vec3 uColor;
 uniform sampler2D uTexture;
 
+varying vec2 vUv;
+
 // varying float vRandom;
 
 void main() {
-    vec4 textureColor = texture2D(uTexture,);
+    vec4 textureColor = texture2D(uTexture, vUv);
     gl_FragColor = vec4(uColor, 1.0);
 }
