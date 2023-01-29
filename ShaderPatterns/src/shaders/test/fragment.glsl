b51083cdc020832d1d154b0b46801e128bcc4a37
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 13:52:31 +0300

    Used a step instead of an if else statement for the conditional values

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -18,12 +18,7 @@ void main() {
 
     // Gray scale banded pattern with more pronounced banding and some dark separation
     float strength = mod(vUv.y * 10.0, 1.0);
-
-    if(strength < 0.5) {
-        strength = 0.0;
-    } else {
-        strength = 1.0;
-    }
+    strength = step(0.5, strength);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
