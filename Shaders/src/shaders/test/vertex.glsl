b2ffc785f8f2e49f08acd67a059efa98f17e240d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 11:00:26 +0300

    MAde the modelPosition x variable multiplier a variable

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -11,7 +11,7 @@ attribute vec3 position;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += sin(modelPosition.x * uFrequency) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
