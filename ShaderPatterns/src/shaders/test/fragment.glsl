af8544377f479954cd7ff18614d865bba99da5f4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 15:54:45 +0300

    Used a better solution for the banding

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -77,7 +77,7 @@ void main() {
     // float strength = floor(vUv.x * 10.0) / 10.0;
 
     // Better crisp banding on two dimensions
-    float strength = 0.5 * (floor(vUv.x * 10.0) / 10.0 + floor(vUv.y * 10.0) / 10.0);
+    float strength = floor(vUv.x * 10.0) / 10.0 * floor(vUv.y * 10.0) / 10.0;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
