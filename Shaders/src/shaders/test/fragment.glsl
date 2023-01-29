75c56266b9186633a10452a0b60b9cf952732d6f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 11:45:02 +0300

    Applied the texure to the plane mesh

--- Shaders/src/shaders/test/fragment.glsl
+++ Shaders/src/shaders/test/fragment.glsl
@@ -9,5 +9,6 @@ varying vec2 vUv;
 
 void main() {
     vec4 textureColor = texture2D(uTexture, vUv);
-    gl_FragColor = vec4(uColor, 1.0);
+    // gl_FragColor = vec4(uColor, 1.0);
+    gl_FragColor = textureColor;
 }
