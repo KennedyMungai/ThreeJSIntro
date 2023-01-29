880a0f2ec3c40fa48eba9addea7c808c78f25eea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 13:32:42 +0300

    Added a comment to describe the pattern being used

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -1,5 +1,8 @@
 varying vec2 vUv;
 
 void main() {
-    gl_FragColor = vec4(vUv, 1.0, 1.0);
+    // Gray scale banded pattern
+    float strength = vUv.x;
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
