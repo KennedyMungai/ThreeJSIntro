c487d94576c3114042cba95c0a1e1ccb57f51838
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 10:55:15 +0300

    Made the model look like a waving flag

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -10,6 +10,8 @@ attribute float aRandom;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
