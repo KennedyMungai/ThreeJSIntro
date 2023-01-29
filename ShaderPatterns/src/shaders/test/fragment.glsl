6adf73809737420ccbe211be1ea3c39292404ee6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 13:54:03 +0300

    Implemented a new pattern

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -17,8 +17,12 @@ void main() {
     // float strength = mod(vUv.y * 10.0, 1.0);
 
     // Gray scale banded pattern with more pronounced banding and some dark separation
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = step(0.5, strength);
+
+    // Gray scale banded pattern with more pronounced banding and greater dark separation
     float strength = mod(vUv.y * 10.0, 1.0);
-    strength = step(0.5, strength);
+    strength = step(0.75, strength);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
