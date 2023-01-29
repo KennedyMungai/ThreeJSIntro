40fbcf552b492c62e3e708517092e38158835e9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 11:36:38 +0300

    Define dthe uTexture sampler2D variable

--- Shaders/src/shaders/test/fragment.glsl
+++ Shaders/src/shaders/test/fragment.glsl
@@ -1,9 +1,10 @@
 precision mediump float;
 
 uniform vec3 uColor;
+uniform sampler2D uTexture;
 
 // varying float vRandom;
 
 void main() {
-    gl_FragColor = vec4(uColor, 1.0);
+    vec4 text gl_FragColor = vec4(uColor, 1.0);
 }
