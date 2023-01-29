8c7a48451550dd11017ef4a126fd445fba04cda8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 11:11:39 +0300

    Used the y value of the cector 2 as a multiplier

--- Shaders/src/shaders/test/vertex.glsl
+++ Shaders/src/shaders/test/vertex.glsl
@@ -12,7 +12,7 @@ void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     modelPosition.z += sin(modelPosition.x * uFrequency.x) * 0.1;
-    modelPosition.z += sin(modelPosition.y * uFrequency.x) * 0.1;
+    modelPosition.z += sin(modelPosition.y * uFrequency.y) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
