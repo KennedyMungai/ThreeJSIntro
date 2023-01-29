7abfd30341ea5185ada0ae563997155efb951832
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 17:10:47 +0300

    Implemented the weird pattern

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -149,7 +149,11 @@ void main() {
     // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
 
     // A blurry white ring inside a black plane
-    vec2 wavedUv = vec2(vUv.x, vUv.y + sin(vUv.x * 30.0) * 0.1);
+    // vec2 wavedUv = vec2(vUv.x, vUv.y + sin(vUv.x * 30.0) * 0.1);
+    // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+
+    // Amoeba looking blob thing
+    vec2 wavedUv = vec2(vUv.x + sin(vUv.y * 30.0) * 0.1, vUv.y + sin(vUv.x * 30.0) * 0.1);
     float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
