4316c26277ada289725b194494b21405b074eabd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 17:52:46 +0300

    Accomplished the ghosty looking perlin noise

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -212,7 +212,10 @@ void main() {
     // float strength = cnoise(vUv * 10.0);
 
     // Crisper Perlin Noise
-    float strength = step(0.0, cnoise(vUv * 10.0));
+    // float strength = step(0.0, cnoise(vUv * 10.0));
+
+    // Ghosty looking Perlin Noise
+    float strength = 1.0 - abs(cnoise(vUv * 10.0));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
