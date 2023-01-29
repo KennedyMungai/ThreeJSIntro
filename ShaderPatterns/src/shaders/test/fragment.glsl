5decf3652995066e55af872022f57ea3e8205dce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 17:08:48 +0300

    Managed to create teh wavy circle pattern

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -146,7 +146,11 @@ void main() {
     // float strength = step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
 
     // A crisp white ring inside a black plane
-    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+    // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+
+    // A blurry white ring inside a black plane
+    vec2 wavedUv = vec2(vUv.x, vUv.y + sin(vUv.x * 30.0) * 0.1);
+    float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
