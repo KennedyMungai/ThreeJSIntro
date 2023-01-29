aa3d112b847210b317017d68d99162d67b9ad49a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 14:18:45 +0300

    Implemented a new pattern

--- ShaderPatterns/src/shaders/test/fragment.glsl
+++ ShaderPatterns/src/shaders/test/fragment.glsl
@@ -37,8 +37,17 @@ void main() {
     // strength *= step(0.75, mod(vUv.y * 10.0, 1.0));
 
     // White lines on a black surface
-    float strength = step(0.75, mod(vUv.y * 10.0, 1.0));
-    strength -= step(0.75, mod(vUv.x * 10.0, 1.0));
+    // float strength = step(0.75, mod(vUv.y * 10.0, 1.0));
+    // strength -= step(0.75, mod(vUv.x * 10.0, 1.0));
+
+    // White right angles on the mesh
+    float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    barX *= step(0.75, mod(vUv.y * 10.0, 1.0));
+
+    float barY = step(0.4, mod(vUv.y * 10.0, 1.0));
+    barY *= step(0.75, mod(vUv.x * 10.0, 1.0));
+
+    float strength = barX + barY;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
