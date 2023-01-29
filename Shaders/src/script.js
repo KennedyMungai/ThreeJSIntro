7c7fead88984a098bfc8c716c48265165470bf3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-29 11:02:51 +0300

    Changed it back to 10

--- Shaders/src/script.js
+++ Shaders/src/script.js
@@ -44,7 +44,7 @@ const material = new THREE.RawShaderMaterial({
     fragmentShader: testFragmentShader,
     uniforms:
     {
-        uFrequency: { value: 20 }
+        uFrequency: { value: 10 }
     }
 })
 
