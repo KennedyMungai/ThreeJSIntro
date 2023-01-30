3398d2fb5c2aaf8391f358428a4ce89480808ab9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 21:05:30 +0300

    Created a varyng for the vertex

--- AnimatedGalaxy/src/shaders/galaxy/vertex.glsl
+++ AnimatedGalaxy/src/shaders/galaxy/vertex.glsl
@@ -2,6 +2,8 @@ uniform float uSize;
 
 attribute float aScale;
 
+varying vec3 vColor;
+
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
@@ -11,4 +13,6 @@ void main() {
 
     gl_PointSize = uSize * aScale;
     gl_PointSize *= (1.0 / -viewPosition.z);
+
+    vColor = color;
 }
