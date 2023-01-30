74af640740e8d123e952bea4543593d36afb4d58
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 14:17:57 +0300

    Made sure the distinction between the surface color and the depth color is seen

--- RagingSea/src/shaders/water/fragment.glsl
+++ RagingSea/src/shaders/water/fragment.glsl
@@ -4,7 +4,7 @@ uniform vec3 uSurfaceColor;
 varying float vElevation;
 
 void main() {
-    vec3 color = mix(uDepthColor, uSurfaceColor, vElevation);
+    vec3 color = mix(uDepthColor, uSurfaceColor, vElevation * 6.0);
 
     gl_FragColor = vec4(color, 1.0);
 }
