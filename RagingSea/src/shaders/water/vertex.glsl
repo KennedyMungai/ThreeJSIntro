d043f31b0341d878a6f0201ae26854b673821cd7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 12:41:36 +0300

    Refactored the code based on the data type change

--- RagingSea/src/shaders/water/vertex.glsl
+++ RagingSea/src/shaders/water/vertex.glsl
@@ -1,11 +1,11 @@
 uniform float uBigWaveElevation;
-uniform float uBigWaveFrequency;
+uniform vec2 uBigWaveFrequency;
 
 void main() {
     // model matrix
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    float elevation = sin(modelPosition.x * uBigWaveFrequency) * uBigWaveElevation;
+    float elevation = sin(modelPosition.x * uBigWaveFrequency.x) * uBigWaveElevation;
 
     modelPosition.y += elevation;
 
