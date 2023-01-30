f95bfbff2888f55c764e80a308a7e7448c273dab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 12:08:46 +0300

    Wrote the basic code for the vertex shader

--- RagingSea/src/shaders/water/vertex.glsl
+++ RagingSea/src/shaders/water/vertex.glsl
@@ -1,3 +1,12 @@
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    // model matrix
+    vec4 modelPosition = modelMatrix * vec4(position .1 .0);
+
+    // view matrix
+    vec4 viewPosition = viewMarix * modelPosition;
+
+    // projection matrix
+    vec4 projectionPosition = projectionMatrix * viewPosition;
+
+    vec4 gl_Position = projectionPosition;
 }
