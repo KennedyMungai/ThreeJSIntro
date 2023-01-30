dcd03d77be8a54e6b9a24eb43e19f4ba28868f69
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 20:58:31 +0300

    Defined a sharper light point pattern

--- AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
+++ AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
@@ -5,9 +5,14 @@ void main() {
     // strength = 1.0 - strength;
 
     // Diffuse Point
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength *= 2.0;
+    // strength = 1.0 - strength;
+
+    // Light point pattern
     float strength = distance(gl_PointCoord, vec2(0.5));
-    strength *= 2.0;
     strength = 1.0 - strength;
+    strength = pow(strength, 10.0);
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
