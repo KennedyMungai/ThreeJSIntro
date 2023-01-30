ebd593bc08d9c23111d8eef84b3282474f0e41b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 20:54:45 +0300

    working on a disk pattern

--- AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
+++ AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
@@ -1,8 +1,12 @@
 void main() {
     // Disc
+    // float strength = distance(gl_PointCoord, vec2(0.5));
+    // strength = step(0.5, strength);
+    // strength = 1.0 - strength;
+
+    // Diffuse Point
     float strength = distance(gl_PointCoord, vec2(0.5));
-    strength = step(0.5, strength);
-    strength = 1.0 - strength;
+    strength *= 2.0;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
