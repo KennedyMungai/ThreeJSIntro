760c2eeb3dae8dd2f18382397f0faaac1a209c9a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 12:47:12 +0300

    Applied the wavy action to vectors on the z axis

--- RagingSea/src/shaders/water/vertex.glsl
+++ RagingSea/src/shaders/water/vertex.glsl
@@ -5,7 +5,7 @@ void main() {
     // model matrix
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    float elevation = sin(modelPosition.x * uBigWaveFrequency.x) * uBigWaveElevation;
+    float elevation = sin(modelPosition.x * uBigWaveFrequency.x) * sin(modelPosition.z * uBigWaveFrequency.y) * uBigWaveElevation;
 
     modelPosition.y += elevation;
 
