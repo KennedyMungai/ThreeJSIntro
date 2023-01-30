cf79d47ee80654a5efb929366bb7d452e53b4da7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 14:24:59 +0300

    Implemented the variables on the shader

--- RagingSea/src/shaders/water/fragment.glsl
+++ RagingSea/src/shaders/water/fragment.glsl
@@ -1,10 +1,12 @@
 uniform vec3 uDepthColor;
 uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
 
 varying float vElevation;
 
 void main() {
-    vec3 color = mix(uDepthColor, uSurfaceColor, vElevation);
+    vec3 color = mix(uDepthColor, uSurfaceColor, vElevation * uColorMultiplier + uColorOffset);
 
     gl_FragColor = vec4(color, 1.0);
 }
