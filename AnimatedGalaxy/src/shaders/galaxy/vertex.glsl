e84731a9a0139b2fcd694dbf06eeeba26039a173
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 21:31:36 +0300

    Defined the spin

--- AnimatedGalaxy/src/shaders/galaxy/vertex.glsl
+++ AnimatedGalaxy/src/shaders/galaxy/vertex.glsl
@@ -7,6 +7,10 @@ varying vec3 vColor;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    // Spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
