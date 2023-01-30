9768a422348f77a9fd627d499833f8f8b74ac0e0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 20:55:59 +0300

    Inverted the pattern to get a diffuse point

--- AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
+++ AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
@@ -7,6 +7,7 @@ void main() {
     // Diffuse Point
     float strength = distance(gl_PointCoord, vec2(0.5));
     strength *= 2.0;
+    strength = 1.0 - strength;
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
