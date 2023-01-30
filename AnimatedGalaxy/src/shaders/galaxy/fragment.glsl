77e28f05e2ebcb93c283c2f3cf096bc727d201e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 20:47:10 +0300

    Created a ring pattern on each of the particles

--- AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
+++ AnimatedGalaxy/src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,6 @@
 void main() {
-    gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+    // Disc
+    float strength = distance(gl_PointCoord, vec2(0.5));
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
 }
