fa5f8b44a426ec01ecdcd7a25804d4d9ae3a0e35
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 14:48:19 +0300

    Reduced the magnitude of the waves

--- RagingSea/src/shaders/water/vertex.glsl
+++ RagingSea/src/shaders/water/vertex.glsl
@@ -92,7 +92,7 @@ void main() {
 
     float elevation = sin(modelPosition.x * uBigWaveFrequency.x + (uTime * uBigWavesSpeed)) * sin(modelPosition.z * uBigWaveFrequency.y + (uTime * uBigWavesSpeed)) * uBigWaveElevation;
 
-    elevation += cnoise(vec3(modelPosition.xz * 3.0, uTime * 0.2));
+    elevation += cnoise(vec3(modelPosition.xz * 3.0, uTime * 0.2)) * 0.3;
 
     modelPosition.y += elevation;
 
