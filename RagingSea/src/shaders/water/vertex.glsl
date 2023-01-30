80b5fc2fd94e255e292259db06395b7b38e75ca8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-30 12:26:15 +0300

    Implemented a simple wave pattern on the mesh

--- RagingSea/src/shaders/water/vertex.glsl
+++ RagingSea/src/shaders/water/vertex.glsl
@@ -2,6 +2,8 @@ void main() {
     // model matrix
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+    modelPosition.y += sin(modelPosition.x * 10.0) * 0.1;
+
     // view matrix
     vec4 viewPosition = viewMatrix * modelPosition;
 
