4b6dd920419c6652720b67cb3462b2f62ab26f83
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-31 12:42:41 +0300

    Made the FragColor show red instead of white

--- PostProcessing/src/script.js
+++ PostProcessing/src/script.js
@@ -299,7 +299,7 @@ const TintShader = {
     fragmentShader: `
         void main()
         {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
         }
     `
 }
