bb733f90b02da62a969db3f9fb3afe275d630ed6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-01-31 11:05:55 +0300

    Added an import for the DotScreenPass

--- PostProcessing/src/script.js
+++ PostProcessing/src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass'
 import * as dat from 'dat.gui'
 
 /**
