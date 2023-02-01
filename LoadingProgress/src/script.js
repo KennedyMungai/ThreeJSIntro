442b50dc13803f995aa9657feb7d0c5cdcf98639
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-01 10:49:31 +0300

    Started afresh in the progress callback function

--- LoadingProgress/src/script.js
+++ LoadingProgress/src/script.js
@@ -18,9 +18,7 @@ const loadingManager = new LoadingManager(
     // Progress (The params are itemsUrl, itemsLoaded and itemsTotal)
     (itemsUrl, itemsLoaded, itemsTotal) => 
     {
-        // console.log(Math.floor((itemsLoaded / itemsTotal) * 100) + '%')
-        console.log(itemsLoaded / itemsTotal)
-        loadingBar.style.transform = ((itemsLoaded / itemsTotal), 0)
+        const progressRatio = itemsLoaded / itemsTotal
     },
     // Error
     () => 
