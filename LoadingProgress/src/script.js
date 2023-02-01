cb6b320ff022d2b8760e71cf94a6fd94c3743191
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-01 09:36:25 +0300

    Doubled the size of the plane

--- LoadingProgress/src/script.js
+++ LoadingProgress/src/script.js
@@ -24,7 +24,7 @@ const scene = new THREE.Scene()
 /**
  * Overlay
  */
-const overlayGeometry = new THREE.PlaneBufferGeometry(1, 1, 1, 1)
+const overlayGeometry = new THREE.PlaneBufferGeometry(2, 2, 1, 1)
 const overlayMaterial = new THREE.ShaderMaterial({
     vertexShader: `
         void main()
