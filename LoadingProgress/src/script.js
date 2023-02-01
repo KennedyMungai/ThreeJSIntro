3bbef4a34bbc5ddce22eb39c42570d77b9d2b285
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-01 09:16:16 +0300

    Added a fragment shader to the shader material

--- LoadingProgress/src/script.js
+++ LoadingProgress/src/script.js
@@ -31,6 +31,12 @@ const overlayMaterial = new THREE.ShaderMaterial({
         {
             gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         }
+    `,
+    fragmentShader: `
+        void main()
+        {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        }
     `
 })
 const overlayMesh = new THREE.Mesh(overlayGeometry, overlayMaterial)
