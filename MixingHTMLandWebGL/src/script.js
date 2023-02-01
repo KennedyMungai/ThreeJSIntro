f0622d100679c6a24ff3e2fe36f57b566d77cfa4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-01 14:23:55 +0300

    Added an import for a raycaster4

--- MixingHTMLandWebGL/src/script.js
+++ MixingHTMLandWebGL/src/script.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { gsap } from 'gsap'
-import { Vector3 } from 'three'
+import { Raycaster, Vector3 } from 'three'
 
 /**
  * Loaders
